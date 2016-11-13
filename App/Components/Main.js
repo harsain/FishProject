5243d0a5053fb2e96bdb05ace911e365c2f92d1c
Harsain Singh <harsain@gmail.com>
2016-11-13 18:10:44 +1100

    Working Router

--- App/Components/Main.js
+++ App/Components/Main.js
@@ -3,33 +3,77 @@ import React, { Component, PropTypes } from 'react';
 import {
     View,
     Text,
+    TouchableHighlight,
     StyleSheet
 } from 'react-native';
+
+import { 
+    Container,
+    Content,
+    Title,
+    Card,
+    CardItem,
+    CheckBox,
+    Icon,
+    Button
+} from 'native-base';
+
 import Calendar from 'react-native-calendar';
 
 class Main extends Component {
+    constructor(props) {
+        super(props);
+        console.log(this.props);
+    }
+    next() {
+        // this.props.navigator.push({
+        //     title: 'test',
+        //     component: Next
+        // });
+        this.props.navigator.push({
+            title: 'Test',
+            component: Next
+        });
+    }
+
     render(){
+        
         return (
-          <View style={styles.container}>
+            <Container style={styles.container}>
+                <Content>
+                    <View style={{flexDirection:'column', alignItems:'center'}}>
+                        <Calendar
+                            ref="calendar"
+                            weekStart={0}
+                            style={styles.calendar}
+                        />
+                    </View>
 
-          <Calendar
-            ref="calendar"
-            weekStart={0}
-            style={styles.calendar}
-          />
+                    <Button small bordered info onPress={this.next.bind(this)}> Test </Button>
+                </Content>
+            </Container>
+        )
+    }
+};
 
-          <Text>Test the Router</Text>
-          </View>
-          )
-      }
-  };
+class Next extends Component {
+
+    previous() {
+        this.props.navigator.pop();
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Current Scene</Text>
+            </View>
+      )
+  }
+};
 
 const styles = StyleSheet.create({
     container: {
         marginTop: 65,
-        flexDirection: 'row',
-        alignItems: 'center',
-        alignSelf: 'stretch'
     },
     calendar: {
         margin: 10,
